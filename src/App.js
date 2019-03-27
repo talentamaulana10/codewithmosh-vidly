cee309b91ffedac2f1746bc5445c7be8cc1552a4
talentamaulana10 <tatamf.id@gmail.com>
2019-03-27 16:23:36 +0700

    fix the first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <main className="container">
+      <main className="container  ">
         <h1>hello world</h1>
       </main>
     );
