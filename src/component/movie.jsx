a7a8c17af3c44ca81fac083e1bed7a498a27ec88
talentamaulana10 <tatamf.id@gmail.com>
2019-04-04 11:23:19 +0700

    Pagination- Paginating Data

--- src/component/movie.jsx
+++ src/component/movie.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
 import Like from "./common/like";
 import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
@@ -28,10 +29,11 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage } = this.state;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
 
     if (count === 0)
       return <h1 className="body1">Tidak ada film dalam database!!!!!!!!!!</h1>;
+    const movies = paginate(allMovies, currentPage, pageSize);
     return (
       <React.Fragment>
         <h3 className="body">
@@ -50,7 +52,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map(movie => (
+            {movies.map(movie => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
